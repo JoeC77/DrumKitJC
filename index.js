413b0cad0d118d5dc7198b120ec46266c708e9fa
Jose <josecorvilla_0@hotmail.com>
2022-10-20 20:25:40 -0600

    Drum Kit Website First Commit

---
+++ index.js
@@ -0,0 +1,78 @@
+document.querySelector('.w.drum').addEventListener("click", handleClick);
+
+document.querySelector('.a.drum').addEventListener("click", handleClick);
+
+document.querySelector('.s.drum').addEventListener("click", handleClick);
+
+document.querySelector('.d.drum').addEventListener("click", handleClick);
+
+document.querySelector('.j.drum').addEventListener("click", handleClick);
+
+document.querySelector('.k.drum').addEventListener("click", handleClick);
+
+document.querySelector('.l.drum').addEventListener("click", handleClick);
+
+function handleClick() {
+  var buttonLetter = this.innerHTML;
+  makeSound(buttonLetter);
+  buttonAnimation(buttonLetter);
+}
+
+document.addEventListener("keypress", function(event){
+  var pressedKey = event.key;
+  makeSound(pressedKey);
+  buttonAnimation(pressedKey);
+});
+
+
+function makeSound(eventThatHappened) {
+
+  switch (eventThatHappened) {
+    case "w":
+      var audio = new Audio('sounds/tom-1.mp3');
+      audio.play();
+      break;
+
+    case "a":
+      var audio = new Audio('sounds/tom-2.mp3');
+      audio.play();
+      break;
+
+    case "s":
+      var audio = new Audio('sounds/tom-3.mp3');
+      audio.play();
+      break;
+
+    case "d":
+      var audio = new Audio('sounds/tom-4.mp3');
+      audio.play();
+      break;
+
+    case "j":
+      var audio = new Audio('sounds/crash.mp3');
+      audio.play();
+      break;
+
+    case "k":
+      var audio = new Audio('sounds/kick-bass.mp3');
+      audio.play();
+      break;
+
+    case "l":
+      var audio = new Audio('sounds/snare.mp3');
+      audio.play();
+      break;
+    default:
+  }
+}
+
+function buttonAnimation(currentKey){
+  var activeButton = document.querySelector("." + currentKey);
+
+  activeButton.classList.add('pressed');
+
+  setTimeout(function(){
+    activeButton.classList.remove('pressed');
+  }, 100);
+
+}
